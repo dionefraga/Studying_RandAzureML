2ef4bb8457337fcd36df490715f506708116210c
Dione Luis S. Fraga <dionefraga@gmail.com>
2019-02-05 15:45:26 -0200

    Update Cap.04 - Linguagem R - Gráficos.R

--- Cap.04 - Linguagem R - Gráficos.R
+++ Cap.04 - Linguagem R - Gráficos.R
@@ -6,16 +6,23 @@
 # Configurando o diretório de trabalho
 # Coloque entre aspas o diretório de trabalho que você está usando no seu computador
 # Não use diretórios com espaço no nome
-setwd("C:/FCD/BigDataRAzure/Cap04")
+setwd("C:/Users/ses01816300012/Documents/GitHub/Studying_RandAzureML")
 getwd()
 
 # Exercicio 1 - Crie uma função que receba os dois vetores abaixo como parâmetro, 
 # converta-os em um dataframe e imprima no console
 vec1 <- (10:13)
 vec2 <- c("a", "b", "c", "d")
+conv_to_df = function(vet1, vet2){
+  df = data.frame(cbind(vet1,vet2))
+  print(df)
+}
 
 # Exercicio 2 - Crie uma matriz com 4 linhas e 4 colunas preenchida com 
 # números inteiros e calcule a média de cada linha
+mat <- matrix(data = c(1:16), nrow = 4, ncol = 4 )
+print(mat)
+apply(mat, 1, mean)
 
 
 # Exercicio 3 - Considere o dataframe abaixo. 
@@ -25,10 +32,17 @@ escola <- data.frame(Aluno = c('Alan', 'Alice', 'Alana', 'Aline', 'Alex', 'Ajay'
                      Geografia = c(100, 78, 86, 90, 98, 67),
                      Quimica = c(76, 56, 89, 90, 100, 87))
 
+apply(escola[,c(2,3,4)], 2, mean)
+apply(escola[,c(2), drop = F], 2, mean)
+
+
 # Exercicio 4 - Cria uma lista com 3 elementos, todos numéricos 
 # e calcule a soma de todos os elementos da lista
+lista = list(a=1:4, b=22:30, c=10:14)
+do.call(sum,lista)
 
 # Exercicio 5 - Transforme a lista anterior um vetor
+unlist(lista)
 
 # Exercicio 6 - Considere a string abaixo. Substitua a palavra "textos" por "frases"
 str <- c("Expressoes", "regulares", "em linguagem R", 
@@ -37,23 +51,34 @@ str <- c("Expressoes", "regulares", "em linguagem R",
          "como por exemplo",
          "10992451280")
 
+gsub("textos", "frases", str)
+
 # Exercicio 7 - Usando o dataset mtcars, crie um gráfico com ggplot do tipo 
 # scatter plot. Use as colunas disp e mpg nos eixos x e y respectivamente
+install.packages("ggplot2")
+library(ggplot2)
+ggplot(data = mtcars, aes(x = disp, y = mpg)) + geom_point() #used to create scatter plots
 
 # Exercicio 8 - Considere a matriz abaixo.
 # Crie um bar plot que represente os dados em barras individuais.
 mat1 <- matrix(c(652,1537,598,242,36,46,38,21,218,327,106,67), nrow = 3, byrow = T)
 mat1
 
+barplot(mat1, beside = T)
+
 # Exercício 9 - Qual o erro do código abaixo?
 data(diamonds)
-ggplot(data = diamonds, aes(x = price, group = fill, fill = cut)) + 
+View(diamonds)
+ggplot(data = diamonds, aes(x = price, group = #fill
+                                                cut
+                                     , fill = cut)) + 
   geom_density(adjust = 1.5)
 
 
 # Exercício 10 - Qual o erro do código abaixo?
 ggplot(mtcars, aes(x = as.factor(cyl), fill = as.factor(cyl))) + 
-  geom_barplot() +
+  #geom_barplot() +
+  geom_bar() +
   labs(fill = "cyl")
 
 
